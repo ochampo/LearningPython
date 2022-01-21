11ee479f16eb4327b0d9d34a6b5e8c9557dc8ba3
Daniel Ocampo <ochampo77@gmail.com>
2022-01-20 19:10:47 -0800

    string experimenting

---
+++ StringsInPython/String.py
@@ -0,0 +1,7 @@
+print("Today is a good day to learn Python")
+greeting = "hello"
+name = "Dan Test"
+
+name2 = input("Please enter your name")
+
+print(name + name2 + "  Blah ")
---
+++ StringsInPython/splitString.py
@@ -0,0 +1,4 @@
+splitstring = "this string has been \n split over \n serveral times \n lines"
+tabstring = "\tthis \tstring \t has a lot of tabs"
+print(splitstring)
+print(tabstring)
