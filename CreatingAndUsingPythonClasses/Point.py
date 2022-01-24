99f367ee43b0257a01d44252171e6233776d61dd
Daniel Ocampo <ochampo77@gmail.com>
2022-01-23 22:01:19 -0800

    experimenting with classes in python

---
+++ CreatingAndUsingPythonClasses/Point.py
@@ -0,0 +1,24 @@
+class Point:
+    def __init__(self, x, y ):
+        self.x = y
+        self.y = y
+
+
+class House:
+
+    def __init__(self, wall_area):
+        self.wall_area = wall_area
+       
+        
+class Paint:
+
+    def __init__(self, buckets, color):
+        self.color = color
+        self.buckets = buckets
+
+
+
+point1 = Point(1,2)
+
+
+print(point1.x)
---
+++ ListAndTuples/ListExamples.py
@@ -0,0 +1,6 @@
+Computer_Parts = ["Cpmputer", "Monitor", "KeyBoard", "Mouse", "Mouse Matt"]
+
+for part in Computer_Parts:
+    print(part)
+
+print(part[2])
