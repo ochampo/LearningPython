73afcf974bb256d4f3cc18e8e9009c3a44631e44
Daniel Ocampo <ochampo77@gmail.com>
2022-01-26 23:49:09 -0800

    pop the first element of a linked list.

--- DataStructuresAndAlgorithms/LinkedList/LinkedList.py
+++ DataStructuresAndAlgorithms/LinkedList/LinkedList.py
@@ -40,18 +40,45 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.head = new_node
+    def Pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+
+
+
 
 print('hello')
+
 my_linked_list = LinkedList(19)
+
 my_linked_list.append(11)
 my_linked_list.append(15)
 my_linked_list.append(20)
+my_linked_list.prepend(41)
+
 my_linked_list.print_list()
 
 print("hello")
 print(my_linked_list.pop().value, "pop")
 
-
+print(my_linked_list.head.value)
 
 
     
