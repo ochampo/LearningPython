f0251d2ba6b6987f8c42db5e9c084adfde08cadd
Daniel Ocampo <ochampo77@gmail.com>
2022-02-01 00:14:00 -0800

    added the remove in a linked list

--- DataStructuresAndAlgorithms/LinkedList/DoublyLinked.py
+++ DataStructuresAndAlgorithms/LinkedList/DoublyLinked.py
@@ -117,6 +117,25 @@ class DoublyLinkedList:
 
         return True
 
+
+    def remove(self,value,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+
+        if index == self.length -1 :
+            return self.pop()
+            
+        temp = self.get(index)
+        temp.next.prev.next = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+        self.length -= 1
+        return temp
+
+
       
 
 
