362b8111ea8bdab1d3a3357e0d4e52da66f4f2bb
Daniel Ocampo <ochampo77@gmail.com>
2022-08-30 15:53:28 -0700

    messing arround with built in function in python

---
+++ UseFulBuiltInFunctions/MathFunction.py
@@ -0,0 +1,18 @@
+from ast import Pow
+from pstats import SortKey
+import math
+
+
+Exp = Pow(2,5)
+abs(-50)
+round(1.22)
+
+points = [0,212,2332,1231,3242,12313,1231,23454647,3452234123]
+
+SortKey(points)
+
+print(math.pi)
+
+math.factorial(4)
+math.sqrt(18)
+math.gcd(8,12)
