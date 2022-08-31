ab2cc7c980234d61ed9b43677af73527abb9aa21
Daniel Ocampo <ochampo77@gmail.com>
2022-08-30 23:40:05 -0700

    experimenting with infinite loops in python

--- UseFulBuiltInFunctions/MathFunction.py
+++ UseFulBuiltInFunctions/MathFunction.py
@@ -1,18 +1,29 @@
 from ast import Pow
 from pstats import SortKey
 import math
+import statistics
+import itertools
 
 
-Exp = Pow(2,5)
+pow(2, 5)
 abs(-50)
 round(1.22)
 
 points = [0,212,2332,1231,3242,12313,1231,23454647,3452234123]
 
-SortKey(points)
+points.sort()
 
 print(math.pi)
 
 math.factorial(4)
 math.sqrt(18)
 math.gcd(8,12)
+
+ageData = [10,13,14,12,11,10,15]
+statistics.mode(ageData)
+statistics.mean(ageData)
+
+for x in itertools.count():
+    print(x)
+    if x == 60:
+        break;
