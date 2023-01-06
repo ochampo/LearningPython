14ae9a91019077e74bf3a527f6022816308d4a3a
Daniel Ocampo <danocampo@meta.com>
2023-01-06 00:11:05 -0800

    have to add the chrome driver in the same folder.

--- webScrapping/SeleniumExample.py
+++ webScrapping/SeleniumExample.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path = '.\chromedriver.exe')
 
 driver.get('https://testpages.herokuapp.com/styled/validation/input-validation.html')
+
+messageField = driver.find_element_by_xpath('//*[@id="firstname"]')
+messageField.send_keys('Dan')
