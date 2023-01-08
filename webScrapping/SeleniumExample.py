784a154b1f373ab7919422399d20ab06733f2693
Daniel Ocampo <danocampo@meta.com>
2023-01-07 23:58:21 -0800

    testing drag and drop currently not working,

--- webScrapping/SeleniumExample.py
+++ webScrapping/SeleniumExample.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome(executable_path = '.\chromedriver.exe')
+# driver = webdriver.Chrome(executable_path = '.\chromedriver.exe')
 
-driver.get('https://testpages.herokuapp.com/styled/validation/input-validation.html')
+# driver.get('https://testpages.herokuapp.com/styled/validation/input-validation.html')
 
-messageField = driver.find_element_by_xpath('//*[@id="firstname"]')
-messageField.send_keys('Dan')
+# messageField = driver.find_element_by_xpath('//*[@id="firstname"]')
+# messageField.send_keys('Dan')
+
+
+##### drag example 
+
+driver2 = webdriver.Chrome(executable_path = '.\chromedriver.exe')
+driver2.get('http://www.seleniumframework.com/Practiceform/')
+source = driver2.find_element_by_xpath('//*[@id="draga"]')
+dest = driver2.find_element_by_xpath('//*[@id="dragb"]')
+
+
+actions = ActionChains(driver2)
+
+actions.drag_and_drop(source,dest).perform()
